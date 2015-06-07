fa2a7b83e749c95669f72fe8dd58ff9c01fd01c5
jrs <jrs@b6fe55d2-9eac-11e4-85bb-03a5b7538b46>
2015-06-07 19:12:52 +0000

    completed implementation section (including some patches to the source code to eliminate repitition and fix a sizing bug)
    git-svn-id: https://opendesignengine.net/svn/yavin-thruster/trunk@7 b6fe55d2-9eac-11e4-85bb-03a5b7538b46

--- chamber/pressure_vessel_calcs.py
+++ chamber/pressure_vessel_calcs.py
@@ -19,7 +19,7 @@ class PressureVessel:
     # Variables used to calculate wall thickness
     ri = None                 # inner radius, needs a value assigned
     ro = None                 # outer radius, calculated from inner radius and thickness
-    r = None                  # Radius defined whether experiencing tension or compression
+    r = None                  # radius of interest in the stress calculations, generally set to position of max stress (ri for tension, r0 for compression)
     t = None                  # initial guess for thickness, needs value assigned
     p_c = None                # chamber pressure, needs value assigned
     p_amb = None              # ambient pressure, needs value assigned
@@ -28,43 +28,40 @@ class PressureVessel:
 
     def __init__(self, ri, t, p_c, p_amb, material_strength):
         self.ri = ri
-        self.t = t
+        self.t = t            # TODO: look at automatically selecting a guess, maybe based on thick-walled theory (ri/t >= 10)
         self.ro = ri + t
-        self.r = ri     # ri in tension and r0 in compression, we're always in tension here, ri = max stress
+        self.r = ri
         self.p_c = p_c
         self.p_amb = p_amb
         self.material_strength = material_strength
 
     def calculate_wall_thickness(self):
-        """Solves for the optimal wall thickness based on the inputs given"""
+        """Solves for the desired wall thickness based on the inputs given"""
 
         step_size = 0.001  # Solver step size to find optimal thickness
         stress_limit = self.material_strength  # TODO: Are these terms really interchangeable like this?
 
-        max_stress = self.fs * max(self.sigma_tan(self.ri, self.ro, self.r, self.p_c, self.p_amb),
-                                   self.sigma_rad(self.ri,  self.ro, self.r, self.p_c, self.p_amb)) / 1000.0
-
         # TODO: Do we want to modify copies of ro and t to keep the originals for reference?
         # TODO: The advantange of modifying the class variables is that you end up with a PressureVessel object with
         # TODO: optimized ro and t values that you can access directly, such as pv.ro and pv.t
-        if max_stress < stress_limit:  # Our vessel can handle more stress, so gradually decrease the thickness
-            while max_stress < stress_limit:
+        # TODO: Replace this solver process with something like a binary search
+        if max_stress() < stress_limit:
+            #  Our vessel can handle more stress, so gradually decrease the thickness
+            while max_stress() < stress_limit:
                 self.t -= step_size
 
                 # The new thickness changes our outside radius
                 self.ro = self.ri + self.t
+             
+            self.t += step_size  # the while look exits after one too many steps, add one back
 
-                max_stress = self.fs * max(self.sigma_tan(self.ri, self.ro, self.r, self.p_c, self.p_amb),
-                                           self.sigma_rad(self.ri, self.ro, self.r, self.p_c, self.p_amb)) / 1000.0
         else:
-            while max_stress > stress_limit:  # Our vessel can't handle the stress, gradually increase the thickness
+            # Our vessel can't handle the stress, gradually increase the thickness
+            while max_stress() > stress_limit:  
                 self.t += step_size
 
                 # The new thickness changes our outside radius
                 self.ro = self.ri + self.t
-
-                max_stress = self.fs * max(self.sigma_tan(self.ri, self.ro, self.r, self.p_c, self.p_amb),
-                                           self.sigma_rad(self.ri, self.ro, self.r, self.p_c, self.p_amb)) / 1000.0
 
         return self.t
 
@@ -79,3 +76,7 @@ class PressureVessel:
 
         return (pi * math.pow(ri, 2) - po * math.pow(ro, 2) + math.pow(ri, 2) * math.pow(ro, 2) *
                 (po - pi) / math.pow(r, 2)) / (math.pow(ro, 2) - math.pow(ri, 2))
+    
+    def max_stress(self):
+        return self.fs * max(self.sigma_tan(self.ri, self.ro, self.r, self.p_c, self.p_amb),
+                                            self.sigma_rad(self.ri, self.ro, self.r, self.p_c, self.p_amb))
