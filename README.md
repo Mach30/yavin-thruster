cb78fa08d8969f39ed60e1ff7efb614b04ecb239
csigman <csigman@b6fe55d2-9eac-11e4-85bb-03a5b7538b46>
2015-11-05 02:21:09 +0000

    Removed runtests.py in favor of using Python's built-in unit test discovery, and documented its usage for the uninitiated in the new README.md.
    git-svn-id: https://opendesignengine.net/svn/yavin-thruster/trunk@28 b6fe55d2-9eac-11e4-85bb-03a5b7538b46

---
+++ README.md
@@ -0,0 +1,4 @@
+Unit Tests
+----------
+Unit tests can be executed by using python's built-in unit test discovery and 
+execution. Simply execute `python -m unittest discover`
--- tests/runtests.py
+++
@@ -1,9 +0,0 @@
-from tests import test_cad, test_math
-import unittest
-
-suite = unittest.TestSuite()
-
-suite.addTests(unittest.TestLoader().loadTestsFromTestCase(test_math.TestPressureVesselCalcs))
-suite.addTests(unittest.TestLoader().loadTestsFromTestCase(test_cad.TestHelloWorldChamber))
-
-unittest.TextTestRunner().run(suite)
