8487903aaa6d85e42a7afe81cec7d62d0681b461
jwright <jwright@b6fe55d2-9eac-11e4-85bb-03a5b7538b46>
2015-11-13 04:21:58 +0000

    Made the CAD test conditional based on the version of Python the tests are being run with.
    git-svn-id: https://opendesignengine.net/svn/yavin-thruster/trunk@32 b6fe55d2-9eac-11e4-85bb-03a5b7538b46

--- tests/chamber/test_cad.py
+++ tests/chamber/test_cad.py
@@ -1,6 +1,13 @@
+import sys
 import unittest
 
-
 class TestHelloWorldChamber(unittest.TestCase):
+    req_version = (2,7)
+    cur_version = sys.version_info
+
+    @unittest.skipIf(cur_version > req_version, "Requires Python <= 2.7.x")   
+
     def test_wall_thickness(self):
+        import cadquery
+
         self.assertEqual(1, 1)
