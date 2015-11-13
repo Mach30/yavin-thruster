769d8eddcc18aaab91a58b98a7cb91bd3165e62c
jwright <jwright@b6fe55d2-9eac-11e4-85bb-03a5b7538b46>
2015-11-13 20:49:12 +0000

    Changed test cases to mimic the math tests, using the test case datasets.
    git-svn-id: https://opendesignengine.net/svn/yavin-thruster/trunk@34 b6fe55d2-9eac-11e4-85bb-03a5b7538b46

--- lib/chamber/hello_world_chamber.py
+++ lib/chamber/hello_world_chamber.py
@@ -40,56 +40,48 @@ def mm(value):
         raise ValueError('Must be a length unit type to convert to mm.')
 
 
-# Declare before using pressure vessel calculation library
-ri = 20.0e-3 * units.meter
+class HelloWorldChamber(object):
+    def build(self, ri, t_guess, p_c, p_amb, material_strength, fs, step_size):
+        pv = pressure_vessel_calcs.PressureVessel(ri, t_guess, p_c, p_amb, material_strength, fs, step_size)
+        t = pv.calculate_wall_thickness()
 
-pv = pressure_vessel_calcs.PressureVessel(ri,
-                                          2e-3 * units.meter,
-                                          3.5e6 * units.pascal,
-                                          101325 * units.pascal,
-                                          40e6 * units.pascal,
-                                          2.0,
-                                          0.0001 * units.meter)
-t = pv.calculate_wall_thickness()
+        theta = 45 * units.degree  # The chamber taper angle in degrees
+        total_length = 50.0e-3 * units.meter  # The full length of the chamber exterior
+        converg_sect_length = total_length * 0.35  # Converging section length
 
-theta = 45 * units.degree  # The chamber taper angle in degrees
-total_length = 50.0e-3 * units.meter  # The full length of the chamber exterior
-converg_sect_length = total_length * 0.35  # Converging section length
+        # Where our taper starts at the business end of the chamber
+        theta_start = total_length - converg_sect_length
 
-# Where our taper starts at the business end of the chamber
-theta_start = total_length - converg_sect_length
+        theta_end_point = converg_sect_length * math.tan(theta.to(units.radian))
 
-theta_end_point = converg_sect_length * math.tan(theta.to(units.radian))
+        # Set up some of our re-usable dimensions
+        dtheta = theta_start + converg_sect_length
 
-# Set up some of our re-usable dimensions
-dtheta = theta_start + converg_sect_length
+        # Temp math to get our chamber outline points
+        pointX = dtheta - math.cos(theta.to(units.radian)) * \
+            math.tan(theta.to(units.radian)) * t
+        pointY = theta_end_point - math.sin(theta.to(units.radian)) * \
+            math.tan(theta.to(units.radian)) * t
+        pointY2 = (theta_end_point - t / math.cos(theta.to(units.radian)))
+        diffX = pointX - theta_start
+        diffY = pointY - 0
 
-# Temp math to get our chamber outline points
-pointX = dtheta - math.cos(theta.to(units.radian)) * \
-    math.tan(theta.to(units.radian)) * t
-pointY = theta_end_point - math.sin(theta.to(units.radian)) * \
-    math.tan(theta.to(units.radian)) * t
-pointY2 = (theta_end_point - t / math.cos(theta.to(units.radian)))
-diffX = pointX - theta_start
-diffY = pointY - 0
+        chamber_points = [(mm(theta_start), 0),
+                          (mm(dtheta), mm(theta_end_point)),
+                          (mm(dtheta), mm(pointY2)),
+                          (mm(dtheta - diffX), mm(pointY2 - diffY)),
+                          (mm(theta_start), mm(-t)),
+                          (0, mm(-t))]
 
-chamber_points = [(0, 0),
-                  (mm(theta_start), 0),
-                  (mm(dtheta), mm(theta_end_point)),
-                  (mm(dtheta), mm(pointY2)),
-                  (mm(dtheta - diffX), mm(pointY2 - diffY)),
-                  (mm(theta_start), mm(-t)),
-                  (0, mm(-t)),
-                  (0, 0)]
+        outline = cq.Workplane('XY').polyline(chamber_points).close()
 
-outline = cq.Workplane('XY').polyline(chamber_points)
+        chamber = outline.revolve(360.0, (0, ri.to(units.millimeter).magnitude, 0),
+                                  (1, ri.to(units.millimeter).magnitude, 0))
 
-chamber = outline.revolve(360.0, (0, ri.to(units.millimeter).magnitude, 0),
-                          (1, ri.to(units.millimeter).magnitude, 0))
+        # Rotate the chamber so that it's properly aligned for 3D printing
+        chamber = chamber.rotate((0, 0, 0), (0, 1, 0), -90)
 
-# Rotate the chamber so that it's properly aligned for 3D printing
-chamber = chamber.rotate((0, 0, 0), (0, 1, 0), -90)
+        return chamber
 
-# Show the outline and/or shell
-# show(outline, (204, 204, 204, 0.0))
-show(chamber, (204, 204, 204, 0.0))
+#chamber = HelloWorldChamber().build()
+#show(chamber, (204, 204, 204, 0.0))
--- tests/chamber/test_cad.py
+++ tests/chamber/test_cad.py
@@ -1,13 +1,80 @@
 import sys
 import unittest
+from pint import UnitRegistry
+from lib.chamber import PressureVessel
+from . import PressureVesselTestCaseDataset
 
 class TestHelloWorldChamber(unittest.TestCase):
-    req_version = (2,7)
+    """
+    Handles running tests on the geometry of the 3D models that were created from our pre-defined dataset of
+    sample dimensions.
+    """
+
+    # Allows us to skip this test if the version of Python isn't compatible
+    reqd_version = (2, 100, 100)
     cur_version = sys.version_info
+    version_msg = "Requires Python <= 2.7.x"
 
-    @unittest.skipIf(cur_version > req_version, "Requires Python <= 2.7.x")   
+    def __init__(self, *args, **kwargs):
+        super(TestHelloWorldChamber, self).__init__(*args, **kwargs)
 
-    def test_wall_thickness(self):
-        import cadquery
+        # Our datasets that drive the creation of the geometry
+        self.test_case_dataset = PressureVesselTestCaseDataset()
 
+    # We don't want to run this test case if it's run with Python 3
+    @unittest.skipIf(cur_version > reqd_version, version_msg)
+    def test_pv1(self):
+        self._test_dataset_item(0)
+
+    @unittest.skipIf(cur_version > reqd_version, version_msg)
+    def test_pv2(self):
+        self._test_dataset_item(1)
+
+    @unittest.skipIf(cur_version > reqd_version, version_msg)
+    def test_pv3(self):
+        self._test_dataset_item(2)
+
+    @unittest.skipIf(cur_version > reqd_version, version_msg)
+    def test_pv4(self):
+        self._test_dataset_item(3)
+
+    @unittest.skipIf(cur_version > reqd_version, version_msg)
+    def test_pv5(self):
+        self._test_dataset_item(4)
+
+    @unittest.skipIf(cur_version > reqd_version, version_msg)
+    def test_pv6(self):
+        self._test_dataset_item(5)
+
+    @unittest.skipIf(cur_version > reqd_version, version_msg)
+    def test_pv7(self):
+        self._test_dataset_item(6)
+
+    @unittest.skipIf(cur_version > reqd_version, version_msg)
+    def test_pv8(self):
+        self._test_dataset_item(7)
+
+    @unittest.skipIf(cur_version > reqd_version, version_msg)
+    def test_pv9(self):
+        self._test_dataset_item(8)
+
+    def _test_dataset_item(self, item_number):
+        """
+        Handles constructing and testing the geometry of the HelloWorldChamber based on test a selected test dataset.
+        :param item_number: The index of the dataset to use
+        :return: None
+        """
+        from lib.chamber.hello_world_chamber import HelloWorldChamber
+
+        # We've compartmentalized our test case data so that it can be used to test both the math and the geometry
+        tc_data = self.test_case_dataset[item_number]
+
+        # TODO: Pull the pressure vessel calculation values out of the HelloWorldChamber
+        # TODO: class and have the values passed in.
+        # TODO: Instantiate the HelloWorldChamber instance with the current test case data
+
+        # The solid that we need to test the dimensions of
+        chamber = HelloWorldChamber().build(tc_data.ri, tc_data.t_guess, tc_data.p_c, tc_data.p_amb,
+                                            tc_data.material_strength, tc_data.fs, tc_data.step_size)
+       
         self.assertEqual(1, 1)
